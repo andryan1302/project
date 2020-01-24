ceefca7b057010cfbf90af556f86a774fc580051
=andryan13 <andryan130203@gmail.com>
2020-01-24 11:29:57 +0700

    Menambahkan HMVC

---
+++ application/modules/index/controllers/Index.php
@@ -0,0 +1,10 @@
+<?php
+
+class Login extends MX_Controller
+{
+    public function index()
+    {
+        $this->load->view('login_page');
+    }
+    
+}
---
+++ application/modules/index/views/home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+    saya
+    <form action="ss.php" method="post">
+    username
+    <input type="text" name="username"><br/>
+    Password
+    <input type="password" name="password"><br/>
+    <input type="submit" value="kirim">
+    <input type="reset" value="batal">
+</body>
+</html>
